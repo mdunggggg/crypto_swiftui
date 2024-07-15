9aa6c71fb6b02133f304496c13dbfd017c3e7994
imdung <dungmanh74@gmail.com>
2024-07-15 22:29:41 +0700

    Initial Commit

---
+++ crypto/cryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  cryptoApp.swift
+//  crypto
+//
+//  Created by ImDung on 15/7/24.
+//
+
+import SwiftUI
+
+@main
+struct cryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
